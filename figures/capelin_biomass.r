34b85b16f2a3f3908f899f609619947001388b8a
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2018-12-05 10:44:57 -0330

    Tweak biomass trends plot based on reviewer suggestions

--- figures/capelin_biomass.r
+++ figures/capelin_biomass.r
@@ -2,8 +2,9 @@
 library(cowplot)
 library(scales)
 library(RColorBrewer)
-setwd('D:/Buren_files/MEGA/papersAle/Frank_etal_2016_rebuttal/figures')
-cb <- read.csv('capelin_biomass.csv')
+# setwd('D:/Buren_files/MEGA/papersAle/Frank_etal_2016_rebuttal/figures')
+# cb <- read.csv('capelin_biomass.csv')
+cb <- read.csv('figures/capelin_biomass.csv')
 
 every_nth <- function(x, nth, empty = TRUE, inverse = FALSE) 
 {
@@ -96,3 +97,53 @@ p
 # a <- cb %>% 
 #   group_by(year) %>%
 #   summarise(aa = biomass[area == "tb"] / biomass[area == "offshore"] )
+
+
+
+
+mypalette <- head(viridis::inferno(6), 5)
+
+
+cb <- transform(cb,
+                area = factor(area,levels=c(
+                    "fa",
+                    "ussrfa",
+                    "sa",
+                    "ussrsa",
+                    'tb')))
+
+p <- ggplot(data = cb, aes(x = year, y = biomass, group = area, shape = area, color = area))
+p <- p + geom_linerange(aes(ymin = (lcl), ymax = (ucl)), 
+                        position = position_dodge(width = dg), size = 0.5, na.rm = T,
+                        show.legend = FALSE)
+
+p <- p + geom_line(position = position_dodge(width = dg), size = 0.35, linetype = 1,
+                   show.legend = FALSE)
+
+p <- p + geom_point(position = position_dodge(width = dg), size = 2.5, stroke = 1, fill = "white")
+
+p <- p + scale_y_log10(labels = comma, limits = c(1,10000), breaks = c(1,10,100,1000,10000))
+p <- p + annotation_logticks(sides = "l")
+p <- p + theme_set(theme_cowplot())
+p <- p + labs(x = 'Year', y = 'Capelin biomass (kilotonnes)')
+p <- p + theme(legend.title = element_blank())#, axis.text.x  = element_text(angle=90, vjust=0.5))
+p <- p + scale_color_manual(values =  mypalette[c(3, 5, 2, 4, 1)], 
+                            breaks = c( levels(unique(cb$area))),
+                            labels = c("2J3K Fall Canada", '2J3K Fall USSR', "3L Spring Canada", "3LNO Spring USSR", 'Trinity Bay (3L)'))
+p <- p +   scale_shape_manual(breaks = c( levels(unique(cb$area))),
+                              values = c(16, 15, 21, 22, 18),
+                              labels = c("2J3K Fall Canada", '2J3K Fall USSR', "3L Spring Canada", "3LNO Spring USSR", 'Trinity Bay (3L)'))
+p <- p + scale_x_continuous(breaks = seq(1975, 2015, by = 5))
+p <- p + theme(legend.position = c(0.05, 0.18), 
+               legend.text = element_text(size = 7),
+               legend.key.size = unit(10, 'point'))
+
+
+p
+
+p <- p + guides(shape = guide_legend(override.aes = list(linetype = c(0, 0, 1, 1, 1))))
+
+save_plot("figures/Fig4_v2.png", p, base_aspect_ratio = 1.8) # make room for figure legend)
+
+
+
