68e9146df8816c663e935c2b8382ff782e9ba589
A Buren <aburen@mun.ca>
2019-01-04 00:00:51 -0800

    Fig 4 - aroowheads

--- figures/analysis/Fig4_center_of_gravity_map.R
+++ figures/analysis/Fig4_center_of_gravity_map.R
@@ -1,23 +1,29 @@
-
+# load libraries ----
 library(sf)     
 library(raster)
 library(ggplot2)
+library(shape)
 
+# load data ----
 load("data/Fig1_map_data.RData")
 load("../CenterOfGravity/cgi_data.RData")
 source("R/shadow_text.R")
 
+# redirect bathy to read gebco from current computer ----
 bathy@file@name <- here::here('data-raw/GEBCO/NAFO_GEBCO_data.nc')
 
+# plot limits ----
 xlim <- c(-60, -48)
 ylim <- c(46.5, 54.5)
 
+# define elipse ----
 elipses <- cgi_data$cap_ellipse
 xy <- st_coordinates(cgi_data$cap_center)
 x <- xy[, "X"]
 y <- xy[, "Y"]
 z <- cgi_data$cap_center$year
 
+# map function ----
 cgi_map <- function() {
     
     par(mar = c(2.5, 2.5, 1, 1))
@@ -33,8 +39,9 @@ cgi_map <- function() {
     plot(st_union(elipses), add = TRUE, lty = 3, xlim = xlim, ylim = ylim, axes = TRUE, 
          border = "black")
     plot(na_land["COUNTRY"], add = TRUE, col = "grey", border = "grey50", lwd = 0.5)
-    arrows(x[-length(x)], y[-length(y)], x[-1L], y[-1L], col = cols[-1], length = 0.05)
-    
+   # arrows(x[-length(x)], y[-length(y)], x[-1L], y[-1L], col = cols[-1], length = 0.1)
+    shape::Arrows(x[-length(x)], y[-length(y)], x[-1L], y[-1L], col = cols[-1], arr.type = "triangle", arr.width = 0.15, arr.length = 0.15)
+     
     yr <- 1983
     ind <- which(z == yr)
     shadowtext(x[ind], y[ind], yr, col = cols[ind], pos = 4, offset = 0.2)
@@ -77,14 +84,15 @@ cgi_map <- function() {
 # cgi_map()
 # dev.off()
 
-
+# output map ----
 cols <- rev(viridis::viridis(length(z) + 10)) %>% tail(n = length(z))
-
-png("output/Fig4_center_of_gravity_map-alternative_adb.png", units = "in", res = 600, height = 7, width = 7)
+# * png ----
+png("output/Fig4_center_of_gravity_map.png", units = "in", res = 600, height = 7, width = 7)
 cgi_map()
 dev.off()
 
-# postscript("output/Fig4_center_of_gravity_map-alternative.eps", height = 7, width = 7)
-# cgi_map()
-# dev.off()
+# * eps ----
+postscript("output/Fig4_center_of_gravity_map.eps", height = 7, width = 7)
+cgi_map()
+dev.off()
 
